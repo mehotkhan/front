b902d996e45272e453e95efcd74373ab4f5f89fe
Ali Zemani <mehot1@gmail.com>
2025-03-02 20:31:28 +0330

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,112 +5,64 @@ export default defineAppConfig({
     },
   },
   mainMenu: [
-    [
-      {
-        label: "Home",
-        // icon: "i-lucide-home",
-        to: "/",
-      },
-      {
-        label: "Explorer",
-        // icon: "i-lucide-search",
-        to: "/explorer",
-      },
-      {
-        label: "About",
-        // icon: "i-lucide-info",
-        to: "/about",
-      },
-      {
-        label: "Contact",
-        // icon: "i-lucide-mail",
-        to: "/contact",
-      },
-    ],
-    [
-      {
-        label: "Manage",
-        slot: "manage",
-      },
-      {
-        label: "i18n",
-        slot: "i18n",
-      },
-      {
-        label: "theme",
-        slot: "theme",
-      },
-
-      {
-        label: "avatar",
-        slot: "avatar",
-      },
-    ],
+    {
+      label: "Home",
+      // icon: "i-lucide-home",
+      to: "/",
+    },
+    {
+      label: "Explorer",
+      // icon: "i-lucide-search",
+      to: "/explorer",
+    },
+    {
+      label: "About",
+      // icon: "i-lucide-info",
+      to: "/about",
+    },
+    {
+      label: "Contact",
+      // icon: "i-lucide-mail",
+      to: "/contact",
+    },
   ],
   dashMenu: [
-    [
-      {
-        label: "Dashboard",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage",
-      },
-      {
-        label: "Items",
-        // icon: "i-lucide-book-open",
-        // active: true,
-        defaultOpen: true,
-        to: "/manage/items",
-      },
-      // {
-      //   label: "Comments",
-      //   // icon: "i-lucide-database",
-      //   to: "/manage/comments",
-      // },
-      {
-        label: "Users",
-        // icon: "i-lucide-box",
-        to: "/manage/users",
-        defaultOpen: true,
-      },
-      {
-        label: "Builds",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage/builds",
-      },
-      {
-        label: "Settings",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage/settings",
-      },
-    ],
-    [
-      {
-        label: "Manage",
-        slot: "manage",
-      },
-      {
-        label: "Help",
-        // icon: "i-lucide-circle-help",
-        // disabled: true,
-      },
-      {
-        label: "i18n",
-        slot: "i18n",
-      },
-
-      {
-        label: "theme",
-        slot: "theme",
-      },
-
-      {
-        label: "avatar",
-        slot: "avatar",
-      },
-    ],
+    {
+      label: "Dashboard",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage",
+    },
+    {
+      label: "Items",
+      // icon: "i-lucide-book-open",
+      // active: true,
+      defaultOpen: true,
+      to: "/manage/items",
+    },
+    // {
+    //   label: "Comments",
+    //   // icon: "i-lucide-database",
+    //   to: "/manage/comments",
+    // },
+    {
+      label: "Users",
+      // icon: "i-lucide-box",
+      to: "/manage/users",
+      defaultOpen: true,
+    },
+    {
+      label: "Builds",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage/builds",
+    },
+    {
+      label: "Settings",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage/settings",
+    },
   ],
   corePermissions: [
     "dashboard.read",
