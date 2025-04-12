ee362afb291b97d7db69cef2c84ff6b5214f870e
Ali Zemani <mehot1@gmail.com>
2025-04-13 02:19:03 +0330

    page load ?

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -18,7 +18,7 @@ useSeoMeta({
 <template>
   <div class="w-full min-h-screen">
     <!-- If Content is Available -->
-    <div class="w-full">
+    <div v-if="pageData" class="w-full">
       <template v-if="pageData.thumbnail">
         <div
           class="page-header flex flex-col gap-6 text-center pt-10 pb-12 md:min-h-[calc(100vh-2rem)] items-center justify-between text-gray-600 border-gray-200 bg-gray-100"
@@ -84,6 +84,28 @@ useSeoMeta({
           <ContentRenderer :value="pageData" class="w-full" />
           <Comments v-if="pageData.comments" />
         </div>
+      </UContainer>
+
+      <!-- Edit Button (If User Has Permission) -->
+    </div>
+
+    <div v-else class="flex items-center h-[calc(100vh-7rem)]">
+      <UContainer
+        class="flex flex-col items-center max-w-7xl mx-auto gap-6 text-center"
+      >
+        <UIcon name="i-lucide-search-x" class="text-yellow-500 size-40" />
+        <h2 class="text-3xl font-semibold">{{ $t("404 - Page Not Found") }}</h2>
+        <p class="text-lg">
+          {{
+            $t("The page you are looking for doesn’t exist or has been moved.")
+          }}
+        </p>
+        <p class="text-lg">
+          {{ $t("Please check the URL or return to the homepage.") }}
+        </p>
+        <RouterLink to="/" class="mt-4 text-blue-500 hover:underline">
+          {{ $t("Return to Homepage") }}
+        </RouterLink>
       </UContainer>
     </div>
   </div>
